82fc3052d456a666252123d1f8a90b1e531c6e48
truongforworkff <huntr02202003@gmail.com>
2024-08-15 02:19:18 +0700

    routes slug products

--- routes/products.js
+++ routes/products.js
@@ -5,7 +5,7 @@ const router = express.Router();
 const productController = require('../controllers/productController');
 
 router.get('/', productController.getAllProducts);
-router.get('/:id', productController.getProductById);
+router.get('/:id([0-9a-fA-F]{24})', productController.getProductById);
 router.get('/:slug', productController.getProductBySlug);
 // Thêm sản phẩm mới
 router.post('/', productController.addProduct);
